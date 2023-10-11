a13d054bdc2f44a6165063c4ff2861ab23aa546d
scastillou <sergio.castillo@ucreativa.com>
2023-10-10 20:40:50 -0600

    first commit

---
+++ src/Saludo.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+
+function Saludo(){
+    return(
+        <div>
+            <spam> Buscar </spam>
+            <input type="text" placeholder="Search" /> <input type="button" value={"Search"}/>
+           
+        </div>
+    )
+}
+
+
+export default Saludo;
